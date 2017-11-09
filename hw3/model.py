62db63ceb52a4de9a160ec8f25989c3264dcd353
cmchang <chunming00@icloud.com>
2017-11-10 03:22:48 +0800

    add pre-activation BN

--- hw3/model.py
+++ hw3/model.py
@@ -10,6 +10,8 @@ import keras
 from keras.models import Model
 from keras.layers import Flatten
 from keras.layers import Dense
+from keras.layers import activation
+from keras.layers import BatchNormalization
 from keras.layers import Input
 from keras.layers import Conv2D
 from keras.layers import MaxPooling2D
@@ -20,35 +22,63 @@ from keras import backend as K
 def model(input_shape=None, num_classes=10):
     img_input = Input(shape=input_shape)
     # Block 1
-    x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv1')(img_input)
-    x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv2')(x)
+    x = Conv2D(64, (3, 3), padding='same', name='block1_conv1')(img_input)
+    x = BatchNormalization(x)
+    x = Activation('relu')(x)
+    x = Conv2D(64, (3, 3), padding='same', name='block1_conv2')(x)
+    x = BatchNormalization(x)
+    x = Activation('relu')(x)
     x = MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool')(x)
 
     # Block 2
-    x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv1')(x)
-    x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv2')(x)
+    x = Conv2D(128, (3, 3), padding='same', name='block2_conv1')(x)
+    x = BatchNormalization(x)
+    x = Activation('relu')(x)
+    x = Conv2D(128, (3, 3), padding='same', name='block2_conv2')(x)
+    x = BatchNormalization(x)
+    x = Activation('relu')(x)
     x = MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool')(x)
 
     # Block 3
-    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv1')(x)
-    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv2')(x)
-    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv3')(x)
+    x = Conv2D(256, (3, 3), padding='same', name='block3_conv1')(x)
+    x = BatchNormalization(x)
+    x = Activation('relu')(x)
+    x = Conv2D(256, (3, 3), padding='same', name='block3_conv2')(x)
+    x = BatchNormalization(x)
+    x = Activation('relu')(x)
+    x = Conv2D(256, (3, 3), padding='same', name='block3_conv3')(x)
+    x = BatchNormalization(x)
+    x = Activation('relu')(x)
     x = MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool')(x)
 
     # Block 4
-    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv1')(x)
-    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv2')(x)
-    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv3')(x)
+    x = Conv2D(512, (3, 3), padding='same', name='block4_conv1')(x)
+    x = BatchNormalization(x)
+    x = Activation('relu')(x)
+    x = Conv2D(512, (3, 3), padding='same', name='block4_conv2')(x)
+    x = BatchNormalization(x)
+    x = Activation('relu')(x)
+    x = Conv2D(512, (3, 3), padding='same', name='block4_conv3')(x)
+    x = BatchNormalization(x)
+    x = Activation('relu')(x)
     x = MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool')(x)
 
     # Block 5
-    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv1')(x)
-    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv2')(x)
-    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv3')(x)
+    x = Conv2D(512, (3, 3), padding='same', name='block5_conv1')(x)
+    x = BatchNormalization(x)
+    x = Activation('relu')(x)
+    x = Conv2D(512, (3, 3), padding='same', name='block5_conv2')(x)
+    x = BatchNormalization(x)
+    x = Activation('relu')(x)
+    x = Conv2D(512, (3, 3), padding='same', name='block5_conv3')(x)
+    x = BatchNormalization(x)
+    x = Activation('relu')(x)
     x = MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool')(x)
 
     x = Flatten(name='flatten')(x)
-    x = Dense(512,activation='relu',name='fc1')(x)
+    x = Dense(512,name='fc1')(x)
+    x = BatchNormalization(x)
+    x = Activation('relu')(x)
     x = Dense(num_classes, activation='softmax', name='predictions')(x)
 
     model = Model(img_input, x, name='vgg16')
