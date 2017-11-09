885a67d5491b7393f37b428dc5e25b58194ce051
cmchang <cmchang@iis.sinica.edu.tw>
2017-11-10 03:29:37 +0800

    finalized BN version

--- hw3/model.py
+++ hw3/model.py
@@ -11,7 +11,7 @@ from keras.models import Model
 from keras.layers import Flatten
 from keras.layers import Dense
 from keras.layers import Activation
-from keras.layers import BatchNormalization()
+from keras.layers import BatchNormalization
 from keras.layers import Input
 from keras.layers import Conv2D
 from keras.layers import MaxPooling2D
