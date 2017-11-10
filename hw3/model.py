15626f03c445c8d209564827e04b23c719509432
cmchang <chunming00@icloud.com>
2017-11-11 00:40:34 +0800

    update model.py

--- hw3/model.py
+++ hw3/model.py
@@ -18,7 +18,7 @@ from keras.layers import MaxPooling2D
 from keras.layers import GlobalAveragePooling2D
 from keras.layers import GlobalMaxPooling2D
 from keras import backend as K
-
+#congrats! the last year!!!!! love you waiting for you 
 def model(input_shape=None, num_classes=10):
     img_input = Input(shape=input_shape)
     # Block 1
@@ -31,49 +31,49 @@ def model(input_shape=None, num_classes=10):
     x = MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool')(x)
 
     # Block 2
-    x = Conv2D(128, (3, 3), padding='same', name='block2_conv1')(x)
+    x = Conv2D(64, (3, 3), padding='same', name='block2_conv1')(x)
     x = BatchNormalization()(x)
     x = Activation('relu')(x)
-    x = Conv2D(128, (3, 3), padding='same', name='block2_conv2')(x)
+    x = Conv2D(64, (3, 3), padding='same', name='block2_conv2')(x)
     x = BatchNormalization()(x)
     x = Activation('relu')(x)
     x = MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool')(x)
 
-    # Block 3
-    x = Conv2D(256, (3, 3), padding='same', name='block3_conv1')(x)
-    x = BatchNormalization()(x)
-    x = Activation('relu')(x)
-    x = Conv2D(256, (3, 3), padding='same', name='block3_conv2')(x)
-    x = BatchNormalization()(x)
-    x = Activation('relu')(x)
-    x = Conv2D(256, (3, 3), padding='same', name='block3_conv3')(x)
-    x = BatchNormalization()(x)
-    x = Activation('relu')(x)
-    x = MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool')(x)
+    # # Block 3
+    # x = Conv2D(128, (3, 3), padding='same', name='block3_conv1')(x)
+    # x = BatchNormalization()(x)
+    # x = Activation('relu')(x)
+    # x = Conv2D(128, (3, 3), padding='same', name='block3_conv2')(x)
+    # x = BatchNormalization()(x)
+    # x = Activation('relu')(x)
+    # x = Conv2D(128, (3, 3), padding='same', name='block3_conv3')(x)
+    # x = BatchNormalization()(x)
+    # x = Activation('relu')(x)
+    # x = MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool')(x)
 
-    # Block 4
-    x = Conv2D(512, (3, 3), padding='same', name='block4_conv1')(x)
-    x = BatchNormalization()(x)
-    x = Activation('relu')(x)
-    x = Conv2D(512, (3, 3), padding='same', name='block4_conv2')(x)
-    x = BatchNormalization()(x)
-    x = Activation('relu')(x)
-    x = Conv2D(512, (3, 3), padding='same', name='block4_conv3')(x)
-    x = BatchNormalization()(x)
-    x = Activation('relu')(x)
-    x = MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool')(x)
+    # # Block 4
+    # x = Conv2D(512, (3, 3), padding='same', name='block4_conv1')(x)
+    # x = BatchNormalization()(x)
+    # x = Activation('relu')(x)
+    # x = Conv2D(512, (3, 3), padding='same', name='block4_conv2')(x)
+    # x = BatchNormalization()(x)
+    # x = Activation('relu')(x)
+    # x = Conv2D(512, (3, 3), padding='same', name='block4_conv3')(x)
+    # x = BatchNormalization()(x)
+    # x = Activation('relu')(x)
+    # x = MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool')(x)
 
-    # Block 5
-    x = Conv2D(512, (3, 3), padding='same', name='block5_conv1')(x)
-    x = BatchNormalization()(x)
-    x = Activation('relu')(x)
-    x = Conv2D(512, (3, 3), padding='same', name='block5_conv2')(x)
-    x = BatchNormalization()(x)
-    x = Activation('relu')(x)
-    x = Conv2D(512, (3, 3), padding='same', name='block5_conv3')(x)
-    x = BatchNormalization()(x)
-    x = Activation('relu')(x)
-    x = MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool')(x)
+    # # Block 5
+    # x = Conv2D(512, (3, 3), padding='same', name='block5_conv1')(x)
+    # x = BatchNormalization()(x)
+    # x = Activation('relu')(x)
+    # x = Conv2D(512, (3, 3), padding='same', name='block5_conv2')(x)
+    # x = BatchNormalization()(x)
+    # x = Activation('relu')(x)
+    # x = Conv2D(512, (3, 3), padding='same', name='block5_conv3')(x)
+    # x = BatchNormalization()(x)
+    # x = Activation('relu')(x)
+    # x = MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool')(x)
 
     x = Flatten(name='flatten')(x)
     x = Dense(512,name='fc1')(x)
