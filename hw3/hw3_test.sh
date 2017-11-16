e42a43cb4d4ef9f3ca0ce38a8cd507f9ece40289
cmchang <chunming00@icloud.com>
2017-11-16 23:34:31 +0800

    bash hw3_test.sh <testing file> <prediction file>

--- hw3/hw3_test.sh
+++ hw3/hw3_test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#wget -O save/model.h5 "https://www.dropbox.com/s/20cki2hw8qbz6mm/model.h5?dl=0"
+wget -O save/model.h5 "https://www.dropbox.com/s/20cki2hw8qbz6mm/model.h5?dl=0"
 python3 test.py --filename "$1" --output "$2"
