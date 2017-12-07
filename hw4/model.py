4439b74dc3d28a2b44e17d64607440ba329d8a22
cmchang <cmchang@iis.sinica.edu.tw>
2017-12-08 01:58:47 +0800

    final model with fixed structure

--- hw4/model.py
+++ hw4/model.py
@@ -1,23 +1,45 @@
-from keras.models import Sequential
+# from keras.models import Sequential
 from keras.layers import Embedding,LSTM,Dense,Dropout,Bidirectional
+from keras.layers import Embedding,Input,InputLayer,Dense,Bidirectional,LSTM,Dropout
+from keras.models import Model,load_model
+from six.moves import cPickle
 
 class SentiLSTM():
     def __init__(self, args):
         # model parameters
         self.n_word       = args.n_word
-        self.n_vocab      = args.n_vocab
-        self.dim_word_embed = args.dim_word_embed 
-        self.dim_hidden   = args.dim_hidden
+        
+        if args.w2v_weight is not None:
+            with open(args.w2v_weight, 'rb') as f:
+                self.w2v_weight = cPickle.load(f)
+                print("embedd matrix shape: ",self.w2v_weight.shape)
+            self.dim_word_embed = self.w2v_weight.shape[1]
+            self.n_vocab        = self.w2v_weight.shape[0]
+        else:
+            self.n_vocab        = args.n_vocab
+            self.dim_word_embed = args.dim_word_embed
 
     def build(self):
         print('Build model...')
-        model = Sequential()
-        model.add(Embedding(self.n_vocab, self.dim_word_embed, input_length = self.n_word))
-        model.add(Bidirectional(LSTM(self.dim_hidden, activation="tanh", return_sequences=True),input_shape=(self.n_word, self.dim_word_embed)))
-        #model.add(Dropout(0.5))
-        model.add(Bidirectional(LSTM(int(self.dim_hidden/2), activation="tanh")))
-        model.add(Dropout(0.5))
-        model.add(Dense(128, activation='relu'))
-        model.add(Dropout(0.5))
-        model.add(Dense(2, activation='softmax'))
+        if self.w2v_weight is not None:
+            embedding_layer = Embedding(self.n_vocab,output_dim= self.dim_word_embed,
+                                weights=[self.w2v_weight],
+                                input_length=self.n_word,
+                                trainable=False)
+        else:
+            embedding_layer = Embedding(self.n_vocab,
+                                output_dim= self.dim_word_embed,
+                                input_length=self.n_word)
+        
+        seq_input = Input(shape=(self.n_word,), dtype='int32')
+        seq_embedded = embedding_layer(seq_input)
+
+        x = Bidirectional(LSTM(128, activation="tanh", return_sequences=True, dropout=0.3))(seq_embedded)
+        x = Bidirectional(LSTM(128, activation="tanh", return_sequences=True, dropout=0.3))(x)
+        x = Bidirectional(LSTM(64, activation="tanh", return_sequences=False, dropout=0.3))(x)
+        x = Dense(64, activation='relu')(x)
+        x = Dropout(0.5)(x)
+        output = Dense(2, activation='softmax')(x)
+        model = Model(seq_input,output)
+        model.summary()
         return(model)
