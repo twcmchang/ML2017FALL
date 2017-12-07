802c7cfe44197e1d042631a5114f642ff0f38e03
cmchang <chunming00@icloud.com>
2017-12-07 17:33:54 +0800

    bidirectional LSTM 2 layers

--- hw4/model.py
+++ hw4/model.py
@@ -1,5 +1,5 @@
 from keras.models import Sequential
-from keras.layers import Embedding,LSTM,Dense,Dropout
+from keras.layers import Embedding,LSTM,Dense,Dropout,Bidirectional
 
 class SentiLSTM():
     def __init__(self, args):
@@ -13,8 +13,9 @@ class SentiLSTM():
         print('Build model...')
         model = Sequential()
         model.add(Embedding(self.n_vocab, self.dim_word_embed, input_length = self.n_word))
-        model.add(LSTM(self.dim_hidden, dropout=0.5, recurrent_dropout=0.4))
-        model.add(Dense(256, activation='relu'))
-        model.add(Dropout(0.4))
+        model.add(Bidirectional(LSTM(self.dim_hidden, dropout=0.5, recurrent_dropout=0.4)))
+        model.add(Bidirectional(LSTM(int(self.dim_hidden/2),dropout=0.5, recurrent_dropout=0.4)))
+        model.add(Dense(128, activation='relu'))
+        model.add(Dropout(0.5))
         model.add(Dense(2, activation='softmax'))
         return(model)
