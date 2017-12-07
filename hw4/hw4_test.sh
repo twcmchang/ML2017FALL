e0fcc977cdd68273fbe87983aa7042cbfffc47a7
Chun-Min Chang <d05921027@ntu.edu.tw>
2017-12-07 23:56:33 +0800

    Update hw4_test.sh

--- hw4/hw4_test.sh
+++ hw4/hw4_test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 wget -O model.zip "https://www.dropbox.com/s/wkhhzx9o8eed79l/save.zip?dl=0"
 unzip model.zip -d save/ 
-python3 test.py --test_file "$1" --output "$2" --save_dir save/ --model save/model.h5
+python3 test.py --test_file "$1" --output "$2" --save_dir save/
