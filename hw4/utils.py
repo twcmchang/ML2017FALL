5139bd52f56b80b68246cbf170e6e2e1d2b1d27c
cmchang <cmchang@iis.sinica.edu.tw>
2017-12-08 23:00:28 +0800

    Use keras tokenizer as preprocess tools instead of hand-crafted class, and remove DataGenerator

--- hw4/utils.py
+++ hw4/utils.py
@@ -2,6 +2,10 @@ import os
 import re
 import random
 import numpy as np
+import gensim
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.utils import to_categorical
 
 class DataLoader(object):
     def __init__(self):
@@ -13,55 +17,85 @@ class DataLoader(object):
         self.test_sentence  = None
         self.train_data     = None
         self.test_data      = None
+        self.tokenizer      = None
+        self.embedding_matrix = None
 
-    def __split_padding_sentence(self, sentence, maxlen):    
-        sentence = sentence.lower().split(' ')
-        caplen = len(sentence)
-        if caplen < maxlen:
-            sentence = np.append(np.repeat('<PAD>',maxlen-caplen),sentence)
-        else:
-            sentence = sentence[:maxlen]
-        idx_sentence = []
-        for w in sentence: # append sentences
-            if w == '<PAD>' or w not in self.vocab:
-                idx_sentence.append(0)
-            else:
-                idx_sentence.append(self.vocab[w])   
-        # to numpy array
-        idx_sentence = np.asarray(idx_sentence)
-        return idx_sentence
+    def create_w2v_model(self):
+        total_corpus = self.train_sentence+self.test_sentence+self.augment_sentence
+        total_corpus = [sent.lower().split(" ") for sent in total_corpus]
+        w2v_model = gensim.models.Word2Vec(total_corpus, size=100, window=5, min_count=0, workers=8)
+        emb_size  = len(w2v_model["dog"])
+        print("embedding size", emb_size)
+        print("gensim model vocab size:", len(w2v_model.wv.vocab))
 
-    def __get_padding_sentence(self,batch_sentence, maxlen):
-        return_sentence = []
-        for i in range(len(batch_sentence)):
-            idx_sentence = self.__split_padding_sentence(batch_sentence[i], maxlen)
-            return_sentence.append(idx_sentence)
-        return_sentence = np.vstack(return_sentence).astype(int)
-        return return_sentence
+        vocab_size = None
+        self.tokenizer = Tokenizer(num_words=vocab_size,filters="\n\t")
+        self.tokenizer.fit_on_texts(self.train_sentence+self.test_sentence+self.augment_sentence)
+        print("tokenizer obtained.")
+
+        word_index = self.tokenizer.word_index
+        print('Found %s unique tokens.' % len(word_index))
+
+        oov_count = 0
+        embedding_matrix = np.zeros((len(word_index), emb_size))
+        for word, i in word_index.items():
+            try:
+                embedding_vector = w2v_model.wv[word]
+                embedding_matrix[i] = embedding_vector
+            except:
+                oov_count +=1
+                print(word)
+        print("oov count: ", oov_count)
+
+        print("embedding matrix obtained.")
+        self.embedding_matrix = embedding_matrix
+        
+    def generate_X_train(self, maxlen):
+        train_sentence = self.tokenizer.texts_to_sequences(self.train_sentence)
+        self.train_data  = pad_sequences(train_sentence, maxlen=maxlen)
+        self.train_label = to_categorical(np.asarray(self.train_label))
+        self.train_label = np.array(self.train_label,dtype=int)
+        print("train_data and train_label obtained.")
+
+    def generate_X_test(self,maxlen):
+        test_sentence = self.tokenizer.texts_to_sequences(self.test_sentence)
+        self.test_data = pad_sequences(test_sentence, maxlen=maxlen)
+        print("test_data obtained.")
+
+    def generate_X_augment(self,maxlen):
+        augment_sentence = self.tokenizer.texts_to_sequences(self.augment_sentence)
+        self.augment_data = pad_sequences(augment_sentence,maxlen)
+
+    def augment_X_train(self, maxlen, data , label):
+        self.train_data  = np.concatenate((self.train_data , data),axis=0)
+        self.train_label = np.concatenate((self.train_label, self.__one_hot_encoding(np.array(label),2)),axis=0)
 
     def __clean_str(self,string, rm_mark = False):
         string = re.sub(r"[^A-Za-z0-9(),!?\'\`\.\:]", " ", string)
-        #string = re.sub(r"\'s", " is", string)
-        string = re.sub(r"\'ve", " have", string)
-        string = re.sub(r"n\'t", " not", string)
-        string = re.sub(r"\'re", " are", string)
-        string = re.sub(r"\'d", " would", string)
-        string = re.sub(r"\'ll", " will", string)
-        string = re.sub(r"\s{0,},", " ", string)
-        string = re.sub(r"\s{2,}", " ", string)
-        string = re.sub(r"\(", " \( ", string)
-        string = re.sub(r"\)", " \) ", string)
+        string = re.sub(r"i ' m","im", string)
+        string = re.sub(r"you ' re","youre", string)
+        string = re.sub(r"aren ' t","arent", string)
+        string = re.sub(r"isn ' t","isnt", string)
+        string = re.sub(r"don ' t","dont", string)
+        string = re.sub(r"doesn ' t","doesnt", string)
+        string = re.sub(r"didn ' t","didnt", string)
+        string = re.sub(r"weren ' t","werent", string)
+        string = re.sub(r"wasn ' t","wasnt", string)
+        string = re.sub(r"won ' t","wont", string)
+        string = re.sub(r"wouldn ' t","wouldnt", string)
+        string = re.sub(r"shouldn ' t","shouldnt", string)
+        string = re.sub(r"can ' t","cant", string)
+        string = re.sub(r"ain ' t","aint", string)
+        string = re.sub(r"couldn ' t","couldnt", string)
+        string = re.sub(r"haven ' t","havent", string)
+        string = re.sub(r"hasn ' t","hasnt", string)
+        string = re.sub(r"[^A-Za-z0-9(),!?\'\`\.\:]", " ", string)
         if rm_mark:
-            string = re.sub(r",", " , ", string)
+            string = re.sub(r",", "", string)
             string = re.sub(r"!", "", string)
             string = re.sub(r"\?", "", string)
         return string.lower().strip()
         
-    def __one_hot_encoding(self, arr, num_classes):
-        res = np.zeros((arr.size, num_classes))
-        res[np.arange(arr.size),arr] = 1
-        return(res)
-
     def read_train(self, train_file=None):
         if train_file is not None:
             with open(train_file) as f:
@@ -72,7 +106,7 @@ class DataLoader(object):
                     train_label.append(int(line[0]))
                     train_sentence.append(self.__clean_str(line[1]))
             self.train_sentence = train_sentence
-            self.train_label    = self.__one_hot_encoding(np.array(train_label),2)
+            self.train_label    = train_label
 
     def read_test(self, test_file=None):
         if test_file is not None:
@@ -93,72 +127,3 @@ class DataLoader(object):
                     augment_sentence.append(self.__clean_str(line))
             self.augment_sentence = augment_sentence
     
-    def set_word_vocab(self,vocab):
-        vocab_inv = {}
-        for key in vocab.keys():
-            vocab_inv[vocab[key]] = key
-        self.vocab = vocab
-        self.vocab_inv = vocab_inv
-
-    def build_word_vocab(self, top_k_words = 3000): 
-        sentences = self.train_sentence + self.test_sentence + self.augment_sentence
-        word_counts = {}
-        nsents = 0
-        for sent in sentences:
-            nsents += 1
-            sent = self.__clean_str(sent)
-            for w in sent.lower().split(' '):
-                word_counts[w] = word_counts.get(w, 0) + 1
-        del word_counts[""] # removed the empty string
-        vocab_tmp = [w for w in sorted(word_counts, key=word_counts.get, reverse=True)]
-        print('using top %d' % top_k_words)
-
-        # Build mapping
-        vocab_inv = {}
-        vocab_inv[0] = '<PAD>'
-
-        vocab = {}
-        vocab['<PAD>'] = 0
-
-        idx = 1
-        for i in range(top_k_words-1):
-            vocab[vocab_tmp[i]] = idx+i
-            vocab_inv[idx+i] = vocab_tmp[i]
-
-        self.vocab = vocab
-        self.vocab_inv = vocab_inv
-
-    def generate_X_train(self, maxlen):
-        self.train_data = self.__get_padding_sentence(self.train_sentence,maxlen)
-
-    def generate_X_test(self,maxlen):
-        self.test_data = self.__get_padding_sentence(self.test_sentence,maxlen)
-
-    def generate_X_augment(self,maxlen):
-        self.augment_data = self.__get_padding_sentence(self.augment_sentence,maxlen)
-
-    def augment_X_train(self, maxlen, data , label):
-        self.train_data  = np.concatenate((self.train_data , data),axis=0)
-        self.train_label = np.concatenate((self.train_label, self.__one_hot_encoding(np.array(label),2)),axis=0)
-
-class DataGenerator():
-    # 'Generates data for Keras'
-    def __init__(self, list_x, labels, shuffle = True):
-        self.X = list_x
-        self.Y = labels
-        self.shuffle = shuffle
-    
-    def generate(self,batch_size):
-        while 1:
-            indexes = np.arange(len(self.X))
-            if(self.shuffle):
-                np.random.shuffle(indexes)
-            # Generate batches
-            imax = int(len(indexes)/batch_size)
-            for i in range(imax):
-                # Find list of IDs
-                X = [self.X[k] for k in indexes[i*batch_size:(i+1)*batch_size]]
-                Y = [self.Y[k] for k in indexes[i*batch_size:(i+1)*batch_size]]
-                X = np.array(X)
-                Y = np.array(Y)
-                yield X, Y
