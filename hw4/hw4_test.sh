24fb0b6154e73f5957c9a34759403b6257ac3d9f
cmchang <cmchang@iis.sinica.edu.tw>
2017-12-08 23:42:41 +0800

    bash hw4_test.sh <testing_data> <prediction_file>

--- hw4/hw4_test.sh
+++ hw4/hw4_test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget -O model.zip "https://www.dropbox.com/s/wkhhzx9o8eed79l/save.zip?dl=0"
-unzip model.zip -d save/ 
+wget -O save.zip "https://www.dropbox.com/s/wye7s3fktdk38jr/save.zip?dl=0"
+unzip save.zip -d save/ 
 python3 test.py --test_file "$1" --output "$2" --save_dir save/
