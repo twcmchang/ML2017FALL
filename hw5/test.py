4c18d92100c835fc6bf6d1693fa047199a6c78d4
cmchang <chunming00@icloud.com>
2017-12-10 01:37:34 +0800

    python3 test.py --test_file <test file> --save_dir <model directory> --output <output file>

---
+++ hw5/test.py
@@ -0,0 +1,44 @@
+import numpy as np
+import pandas as pd
+import os
+import sys
+import keras
+import argparse
+from six.moves import cPickle
+from model import MF
+from utils import DataLoader
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--test_file', type=str, default="data/test.csv",
+                        help='testing filename')
+    parser.add_argument('--output', type=str, default=None,
+                        help='output filename')
+    parser.add_argument('--save_dir', type=str, default='save/',
+                        help='save directory')
+    args = parser.parse_args()
+    test(args)
+
+def test(args):
+    d = DataLoader()
+    if args.test_file is not None:
+        d.read_test(test_file=args.test_file)
+    else:
+        sys.exit("Error! Please specify your testing file.")
+
+    if os.path.exists(os.path.join(args.save_dir,"model.h5")):
+        md = keras.models.load_model(os.path.join(args.save_dir,"model.h5"))
+    else:
+        sys.exit("Error! Please specify your model in use.")
+    md.summary()
+
+    y_pred = md.predict(d.test_data)
+    #labels = [np.argmax(y_pred[i]) for i in range(len(y_pred))]
+    output = pd.DataFrame({"id":np.array(range(len(y_pred)))+1,"label":labels})
+    output.to_csv(args.output, index=False)
+    print("Testing completed and saved into %s." % args.output)
+
+if __name__ == '__main__':
+    main()
+
+
