0cbead284d5f8ff1ab6f2abba7ae892d1db69f6b
cmchang <chunming00@icloud.com>
2017-12-10 17:54:48 +0800

    allow to read user and movie files and incorporate into auxiliary data

--- hw5/utils.py
+++ hw5/utils.py
@@ -9,21 +9,88 @@ class DataLoader(object):
         super().__init__()
         self.n_mov          = None
         self.n_usr          = None
+        self.n_aux          = 0
         self.train_data     = None
+        self.train_aux      = None
         self.train_label    = None
+        self.test_aux       = None
         self.test_data      = None
-        
+
+        # optional
+        self.attr_usr       = None
+        self.attr_mov       = None
+
+    def one_hot_encoding(self,arr, num_classes):
+        res = np.zeros((len(arr), num_classes))
+        res[np.arange(len(arr)),arr] = 1
+        return(res)
+
+    def read_user(self, user_file=None):
+        if user_file is not None:
+            with open(user_file) as f:
+                attr_usr = {}
+                _ = f.readline()
+                for line in f:
+                    line = line.split(sep="::")
+                    if line[1] is 'F':
+                        line[1] = 0
+                    else:
+                        line[1] = 1
+                    attr_usr[line[0]] = np.array([line[1],line[2],line[3]],dtype=float)
+            print("Total number of users: %d" % len(attr_usr))
+            self.attr_usr = attr_usr
+            self.n_aux += 3
+
+    def read_movie(self, movie_file=None):
+        with open(movie_file) as f:
+            attr_mov = {}
+            mov_type_dict = {}
+            _ = f.readline()
+            count = 0
+            for line in f:
+                line = line.split(sep="::")
+                year = int(line[1][-5:-1])/1000
+                this_type = []
+                for ty in line[2].split(sep="|"):
+                    if not (ty in mov_type_dict):
+                        mov_type_dict[ty] = count
+                        this_type.append(count)
+                        count = count + 1
+                    else:
+                        this_type.append(mov_type_dict[ty])
+                attr_mov[line[0]] = [year, this_type]
+            # [year, hot_encoding_of_types]    
+            for key,value in attr_mov.items():
+                attr_mov[key] = np.append(value[0],np.sum(self.one_hot_encoding(value[1],count),axis=0))
+            print("There are %d movies in total and can be categorized into %d types." % (len(attr_mov),count))
+            self.attr_mov = attr_mov
+            self.n_aux += (count+1)
+
     def read_train_test(self, train_file=None, test_file=None, classification=False):
         if train_file is not None:
             with open(train_file) as f:
                 train_data = []
+                train_aux = []
                 train_label = []
                 _ = f.readline()
                 for line in f:
+                    aux = np.array([])
+                    line = re.sub('\s+','',line)
                     line = line.split(",")
+                    if self.attr_usr is not None:
+                        aux = np.append(aux,self.attr_usr[line[1]])
+                    if self.attr_mov is not None:
+                        aux = np.append(aux,self.attr_mov[line[2]])
+                        train_aux.append(aux)
                     train_data.append([int(line[1]),int(line[2])])
                     train_label.append(int(line[3])) # rating range: [1,5]
-            self.train_data  = np.array(train_data)
+            self.train_data = np.array(train_data)
+            print(train_data[0])
+
+            if self.attr_usr is not None or self.attr_mov is not None:
+                self.train_aux = np.array(train_aux)
+                print(train_aux[0])
+
             if classification:
                 self.train_label = to_categorical(train_label-1)
             else:
@@ -32,15 +99,28 @@ class DataLoader(object):
         if test_file is not None:
             with open(test_file) as f:
                 test_data = []
+                test_aux = []
                 _ = f.readline() # skip the first header line: id,sentence
                 for line in f:
+                    aux = np.array([])
+                    line = re.sub('\s+','',line)
                     line = line.split(",")
+                    if self.attr_usr is not None:
+                        aux = np.append(aux,self.attr_usr[line[1]])
+                    if self.attr_mov is not None:
+                        aux = np.append(aux,self.attr_mov[line[2]])
+                        test_aux.append(aux)
                     test_data.append([int(line[1]),int(line[2])])
             self.test_data = np.array(test_data)
+            print(test_data[0])
+            if self.attr_usr is not None or self.attr_mov is not None:
+                self.test_aux = np.array(test_aux)
+                print(test_aux[0])
          
-        if train_file is not None or test_file is not None:   
-            self.n_usr = max(np.append(self.train_data[:,0], self.test_data[:,0]))+1
-            self.n_mov = max(np.append(self.train_data[:,1], self.test_data[:,1]))+1
+        if train_file is not None or test_file is not None:
+            self.n_usr = max([self.train_data[i][0] for i in range(len(self.train_data))]+[self.test_data[i][0] for i in range(len(self.test_data))])+1
+            self.n_mov = max([self.train_data[i][1] for i in range(len(self.train_data))]+[self.test_data[i][1] for i in range(len(self.test_data))])+1
+            #self.n_mov = max(np.append(self.train_data[:,1], self.test_data[:,1]))+1
             print("train_data and train_label obtained.")
             print("there are %d users and %d movies in training dataset." % (self.n_usr, self.n_mov))
         else:
@@ -52,7 +132,13 @@ class DataLoader(object):
                 test_data = []
                 _ = f.readline() # skip the first header line: id,sentence
                 for line in f:
+                    aux = np.array([])
+                    line = re.sub('\s+','',line)
                     line = line.split(",")
+                    if self.attr_usr is not None:
+                        aux = np.append(aux,self.attr_usr[line[1]])
+                    if self.attr_mov is not None:
+                        aux = np.append(aux,self.attr_mov[line[2]])
                     test_data.append([int(line[1]),int(line[2])])
             self.test_data = np.array(test_data)
             print("test_data obtained.")
