e4fb95d609c2af2f2bdec795d9a17af6597c2a84
cmchang <chunming00@icloud.com>
2017-12-21 09:54:22 +0800

    add bias term

--- hw5/model.py
+++ hw5/model.py
@@ -20,17 +20,25 @@ class MF():
         self.usr_embedding_matrix = Embedding(self.n_usr, output_dim = self.dim_embed)
         self.mov_embedding_matrix = Embedding(self.n_mov, output_dim = self.dim_embed)
 
+        self.usr_bias = Embedding(self.n_usr, output_dim = 1, embeddings_initializer='zeros')
+        self.mov_bias = Embedding(self.n_mov, output_dim = 1, embeddings_initializer='zeros')
+
         #self.bias = K.variable(value=0.0)
 
         usr_input = Input(shape=(1,),dtype='int32')
         usr_embedding = self.usr_embedding_matrix(usr_input)
         usr_embedding = Flatten()(usr_embedding)
 
+        usr_bias = self.usr_bias(usr_input)
+
         mov_input = Input(shape=(1,),dtype='int32')
         mov_embedding = self.mov_embedding_matrix(mov_input)
         mov_embedding = Flatten()(mov_embedding)
 
+        mov_bias = self.mov_bias(mov_input)
+
         output = Dot(axes=1)([usr_embedding, mov_embedding])
+        output = Add()([output, usr_bias, mov_bias])
         if self.n_aux is not None:
             aux_input = Input(shape=(self.n_aux,), name='aux_input')
             output = Concatenate()([output, aux_input])
